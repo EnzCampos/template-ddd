278b5e67c36308b20ebfb775c845525e3737abe2
Enzo Campos <enzocampos@gmail.com>
2024-07-25 10:47:52 -0300

    feat: application services registration

---
+++ Template.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
+using FluentValidation;
+using Template.Application.Services;
+using Template.Application.Interfaces.Services;
+
+namespace Template.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices (this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddFluentValidationAutoValidation();
+
+            services.AddSingleton<ITokenService, TokenService>();
+            services.AddScoped<IUserContextService, UserContextService>();
+            services.AddScoped<IAuthService, AuthService>();
+
+            return services;
+        }
+    }
+}
